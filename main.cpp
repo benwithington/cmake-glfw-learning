c2221223663f51f5f9e86580459b233888f73287
Ben <benwithington@hotmail.com>
2022-06-01 13:12:14 +0100

    ok

--- main.cpp
+++ main.cpp
@@ -9,8 +9,8 @@ int main()
     if(!glfwInit())
         exit(EXIT_FAILURE);
 
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
  
     window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
     if (!window)
