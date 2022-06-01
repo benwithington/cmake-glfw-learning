90166e0ba38d5dd816c0e697c6114da3acf56b2e
Ben <benwithington@hotmail.com>
2022-06-01 16:30:00 +0100

    glad included and working

--- main.cpp
+++ main.cpp
@@ -1,26 +1,56 @@
 #include <iostream>
-#include "adder.h"
+#include <string>
+
+#include "glad/glad.h"
 #include "GLFW/glfw3.h"
+
+#include "adder.h"
+
+void processInput(GLFWwindow* window);
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 
 int main()
 {
+
     GLFWwindow* window;
 
     if(!glfwInit())
         exit(EXIT_FAILURE);
 
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
- 
-    window = glfwCreateWindow(640, 480, "Simple example", NULL, NULL);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+    std::string title{"Include GLAD"};
+
+    window = glfwCreateWindow(640, 480, title.c_str(), NULL, NULL);
     if (!window)
     {
         glfwTerminate();
         exit(EXIT_FAILURE);
     }
 
+    glfwMakeContextCurrent(window);
+    
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);  
+
     while(!glfwWindowShouldClose(window))
     {
+        //Frame Starts Here
+        processInput(window);
+
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
@@ -30,3 +60,14 @@ int main()
 
     return 0;
 }
+
+void processInput(GLFWwindow* window)
+{
+    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    glViewport(0, 0, width, height);
+} 
